b99d1e764cd4415317f9f6a7a76118e140deea5c
Shrikant Ghuge <shreeghuge1@gmail.com>
2022-06-26 14:54:32 +0530

    added login functionality

--- routes/login.js
+++ routes/login.js
@@ -12,20 +12,29 @@ const jwt = require('jsonwebtoken');
 jwtKey = "jwt";
 //
 
-router.post('', jsonParser, (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    owner.findOne({ name: req.body.name }).then((data) => {
+router.post('', jsonParser, async(req, res) => {
+    try {
+        const data = await owner.findOne({ name: req.body.name });
+        console.log(data)
+        if (!data) {
+            return res.status(404).send({ message: "User Not found." });
+        }
         var decipher = crypto.createDecipher(algo, key);
         var decrypted = decipher.update(data.password, 'hex', 'utf8') + decipher.final('utf8');
-        console.warn('sadsad', decrypted);
-        if (decrypted == req.body.password) {
+        console.warn('sadsad', req.body.name);
+        if (decrypted === req.body.password && req.body.name === data.name) {
             jwt.sign({ data }, jwtKey, { expiresIn: '300s' }, (err, token) => {
                 res.status(201).json({ token });
             })
         } else {
             res.status(401).json("Password not match")
         }
-    }).catch(error => res.json(error))
+        // .then((data) => {
+
+        // }).catch(error => res.json(error))
+    } catch (error) {
+        return res.status(500).send({ message: error.message });
+    }
 });
 
 module.exports = router;
