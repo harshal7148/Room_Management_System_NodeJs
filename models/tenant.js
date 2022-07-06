6a73d7a75e0aaf1526ef74cf26dc69e1a022c307
harshal7148 <harshalparab1@gmail.com>
2022-07-06 18:37:32 +0530

    tenant_changes

--- models/tenant.js
+++ models/tenant.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 let tenantSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    roomNo:String,
     name: String,
     address: String,
     uin: String,
     profilePic: String,
     rentStartDate: Number,
-    // depositAmount:Number,
+    depositAmount:String,
     isActive: Boolean,
-
+    ownerId: mongoose.Schema.Types.ObjectId
 })
 
 module.exports = mongoose.model("tenants", tenantSchema);
--- routes/tenant.js
+++ routes/tenant.js
@@ -1,34 +1,60 @@
 const express = require('express');
 const router = express.Router();
-// Parser
-const bodyParser = require('body-parser');
-let jsonParser = bodyParser.json();
 // Model reference
 const Tenant = require('../models/tenant');
 const mongoose = require('mongoose');
 
 // API's Creation
-/* Post API */
-router.post('', jsonParser, function (req, res) {
+
+/* Post API - Add Tenant */
+router.post('/addTenant/:ownerId', function(req, res) {
     const data = new Tenant({
         _id: mongoose.Types.ObjectId(),
+        roomNo: req.body.roomNo,
         name: req.body.name,
         address: req.body.address,
         uin: req.body.uin,
         profilePic: req.body.profilePic,
         rentStartDate: req.body.rentStartDate,
-      //  depositAmount: req.body.depositAmount,
+        depositAmount: req.body.depositAmount,
         isActive: req.body.isActive,
+        ownerId: req.params.ownerId
     })
-    data.save().then(() => {
-        res.status(201).json(data);
+    data.save().then((res1) => {
+        res.status(201).json(res1);
     }).catch((error) => {
         return res.status(500).send({ message: error.message });
     })
-    
+})
+
+/* Put API - Update Tenant */
+router.put('/updateTenant/:tenantId/:ownerId' , function(req,res) {
+    const data = new Tenant({
+        roomNo: req.body.roomNo,
+        name: req.body.name,
+        address: req.body.address,
+        uin: req.body.uin,
+        profilePic: req.body.profilePic,
+        rentStartDate: req.body.rentStartDate,
+        depositAmount: req.body.depositAmount,
+        isActive: req.body.isActive,
+        ownerId: req.params.ownerId
+    })
+
+    Tenant.updateOne({_id:req.params.tenantId},data).then((res1) => {
+        res.status(201).json(res1);
+    }).catch((error) => {
+        return res.status(500).send({ message: error.message });
+    })
+})
+
+/* Delete API - Delete Tenant */
+router.delete('/deleteTenant/:tenantId/:ownerId' , function(req,res) {
+    Tenant.deleteOne({_id:req.params.tenantId,ownerId: req.params.ownerId}).then((res1) => {
+        res.status(201).json(res1);
+    }).catch((error) => {
+        return res.status(500).send({ message: error.message });
+    })
 })
 
 module.exports = router;
-
-
-
