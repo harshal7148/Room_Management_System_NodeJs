2e17665e14e7c2830bedcc9c811142907d304e46
Shrikant Ghuge <shreeghuge1@gmail.com>
2022-07-29 10:56:52 +0530

    outstanding changes

--- routes/outstanding.js
+++ routes/outstanding.js
@@ -18,7 +18,7 @@ router.get('', (req, res) => {
 
 router.get('/getOutstandingHistory/:ownerId', (req, res) => {
     outstandingHistory.find({}).
-    populate('tenantId', select = ['name', 'profilePic'])
+        populate('tenantId', select = ['name', 'isActive', 'roomNo', 'depositeAmount', 'profilePic'])
         .then(outsandingHistories => {
             res.status(201).json(outsandingHistories);
         }).catch(err => {
@@ -30,45 +30,45 @@ router.post('/calculateOutstanding/:ownerId', (req, res) => {
     tenant.find({}).then(tenant => {
         tenant.forEach(response => {
             outstandingHistory.findOne({ tenantId: response._id }).
-            then(oustandingRes => {
-                if (stringUtil.isNullorEmpty(oustandingRes)) {
-                    const result = dateUtil.calcNextMonthDate(response.rentStartDate, 30)
-                    if (result.isValid) {
-                        const data = new outstandingHistory({
-                            _id: mongoose.Types.ObjectId(),
-                            ownerId: req.params.ownerId,
-                            tenantId: response._id,
-                            histories: [{
-                                fromDate: response.rentStartDate,
-                                toDate: result.nextDate,
-                                isPaid: false,
-                                paidAmount: '5000',
-                            }]
-                        })
-                        data.save().then(() => {})
-                            .catch((error) => {
-                                return res.status(500).send({ message: error.message });
-                            })
-                    }
-                } else {
-                    outstandingHistory.find({ tenantId: response._id }).then((response) => {
-                        const result = dateUtil.calcNextMonthDate(response[0].histories[response[0].histories.length - 1].toDate)
+                then(oustandingRes => {
+                    if (stringUtil.isNullorEmpty(oustandingRes)) {
+                        const result = dateUtil.calcNextMonthDate(response.rentStartDate, 30)
                         if (result.isValid) {
-                            response[0].histories.push({
-                                fromDate: response[0].histories[response[0].histories.length - 1].toDate,
-                                toDate: result.nextDate,
-                                isPaid: false,
-                                paidAmount: "5000"
+                            const data = new outstandingHistory({
+                                _id: mongoose.Types.ObjectId(),
+                                ownerId: req.params.ownerId,
+                                tenantId: response._id,
+                                histories: [{
+                                    fromDate: response.rentStartDate,
+                                    toDate: result.nextDate,
+                                    isPaid: false,
+                                    paidAmount: '5000',
+                                }]
                             })
-                            outstandingHistory.updateOne({ _id: oustandingRes._id }, {
-                                $set: {
-                                    histories: response[0].histories
-                                }
-                            }).then()
+                            data.save().then(() => { })
+                                .catch((error) => {
+                                    return res.status(500).send({ message: error.message });
+                                })
                         }
-                    })
-                }
-            })
+                    } else {
+                        outstandingHistory.find({ tenantId: response._id }).then((response) => {
+                            const result = dateUtil.calcNextMonthDate(response[0].histories[response[0].histories.length - 1].toDate)
+                            if (result.isValid) {
+                                response[0].histories.push({
+                                    fromDate: response[0].histories[response[0].histories.length - 1].toDate,
+                                    toDate: result.nextDate,
+                                    isPaid: false,
+                                    paidAmount: "5000"
+                                })
+                                outstandingHistory.updateOne({ _id: oustandingRes._id }, {
+                                    $set: {
+                                        histories: response[0].histories
+                                    }
+                                }).then()
+                            }
+                        })
+                    }
+                })
         })
         res.end("Inserted Sucesfully");
     })
